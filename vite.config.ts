4803323cf105bc544fcdb730e6b453be4df644b5
Noman Afridi <nomanafridi941@gmail.com>
2026-02-10 13:06:54 +0500

    Re-add build target es2022 to fix top-level await issue

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        target: 'es2022'
       }
-    };
-});
